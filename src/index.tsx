e315b6d5033ab9e038fbf737b5dab2245f8cb417
James Michael Hunter Turner <michael@turneratlanta.com>
2025-10-07 20:52:55 -0400

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+TypeScript
+
+    // src/index.tsx
+    import React from 'react';
+    import ReactDOM from 'react-dom/client';
+    import App from './App'; // Importing the App component
+
+    const root = ReactDOM.createRoot(
+      document.getElementById('root') as HTMLElement
+    );
+    root.render(
+      <React.StrictMode>
+        <App /> {/* Rendering the App component */}
+      </React.StrictMode>
+    );
